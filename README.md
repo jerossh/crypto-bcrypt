2f1aabf64b8198be26f47fbf337847eecdd17bf8
jerossh <jerosshen@gmail.com>
2017-05-26 01:48:28 +0800

    完成promise 版本

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 基于 crypto 模块开发的用户注册登录插件
+
+# crypto-bctypt
+
+hash the password  for sign in && hash password for sign up/ 注册密码加密，登录密码比对
+
+## Installation
+
+```bash
+npm install crypto-bctypt --save
+```
+
+## Usage 
+
+sign up／ 注册
+
+```js
+genPaswd(user1.psw).then(hash => {
+  // save the hash
+}).catch(err => deal(err))
+
+```
+
+sign in  ／ 登录
+
+```js
+compare(hash, user1.psw).then( bool => {
+  if (bool) {
+    // just to sigin in}
+  else {
+    // may your pswd is wrong
+  }
+}).catch(err=> deal(err))
+
+```
+
+
+## param
+
+#### genPaswd(pasw, salt, iterations, keylen, digest)
+#### compare(hashedPsw, pasw, salt, iterations, keylen digest)
+```js
+// default
+genPaswd(user1.pasw, 'a password', 1000, 69, 'sha512')
+// default
+compare(hash, user1.psw);
+
+
+// custom 
+const customSalt = 'custom'
+enPaswd(user1.pasw, customSalt)
+// compare need custom too
+compare(hash, user1.psw, customSalt);
+
+```
+
+
+
+
--- index.js
+++ index.js
@@ -1,39 +1,67 @@
-const salt = 'a password';
+const salt_d = 'a password';
+const iterations_d = 1000;
+const keylen_d = 69;
+const digest_d = 'sha512';
 const crypto = require('crypto');
 
-function getPaswd (pasw, cb) {
-  const cipher = crypto.createCipher('aes192', pasw);
-  let encrypted = cipher.update(salt, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  cb(encrypted)
+const isSuoprtES6 = process.version.split('.')[0][1]>5;
+if (!isSuoprtES6) throw new Error('not suport this version node');
+
+function genPaswd (pasw, salt=salt_d, iterations=iterations_d, keylen=keylen_d, digest=digest_d) {
+  return new Promise((resolve, reject) => {
+      // crypto.pbkdf2('pasw', 'salt', 100000, 512, 'sha512', (err, key) => {
+      crypto.pbkdf2(pasw, salt, iterations, keylen, digest, (err, key) => {
+        if (err) reject(new Error(err))
+        // console.log(key.toString('hex'))
+        resolve(key.toString('hex'))
+      });
+  })
 }
 
 
-function compare (pasw, hash , cb) {
-  const decipher = crypto.createDecipher('aes192', pasw);
-  const encrypted = hash;
-  let isMatch = false
-  let err = null
-  try {
-    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-
-    if (decrypted === salt) {
-      isMatch = true
-    } else {
-      isMatch = false
-    }
-
-  } catch (e) {
-    console.log(e);
-    isMatch = false
-    err = new Error(e);
-  }
-
-  cb(err, isMatch)
+function compare (hashedPsw, pasw, salt=salt_d, iterations=iterations_d, keylen=keylen_d, digest=digest_d) {
+  return new Promise((resolve, reject) => {
+    genPaswd(pasw, salt, iterations, keylen, digest).then(key => {
+      resolve(key.toString('hex') == hashedPsw)
+    }).catch(err => reject(new Error(err)));
+  })
+  
 }
 
 module.exports = {
-    getPaswd,
+    genPaswd,
     compare
 }
+
+
+// function getPaswd (pasw, cb) {
+//   const cipher = crypto.createCipher('aes192', pasw);
+//   let encrypted = cipher.update(salt, 'utf8', 'hex');
+//   encrypted += cipher.final('hex');
+//   cb(encrypted)
+// }
+
+
+// function compare (pasw, hash , cb) {
+//   const decipher = crypto.createDecipher('aes192', pasw);
+//   const encrypted = hash;
+//   let isMatch = false
+//   let err = null
+//   try {
+//     let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+//     decrypted += decipher.final('utf8');
+
+//     if (decrypted === salt) {
+//       isMatch = true
+//     } else {
+//       isMatch = false
+//     }
+
+//   } catch (e) {
+//     console.log(e);
+//     isMatch = false
+//     err = new Error(e);
+//   }
+
+//   cb(err, isMatch)
+// }
